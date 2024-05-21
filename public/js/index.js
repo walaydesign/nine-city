1a5c8132644dd52ed587113cc56eefb23f367c0d
youzhen <yl301114@gmail.com>
2024-05-21 11:02:26 +0100

    school

--- public/js/index.js
+++ public/js/index.js
@@ -33,6 +33,19 @@ var swiperMuseum = new Swiper(".museum_swiper", {
     }
 })
 
+var swiperNature = new Swiper(".nature_swiper", {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+    },
+    scrollbar: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    speed: 800,
+})
+
 var swiperSave = new Swiper(".save_swiper", {
     slidesPerView: 1,
     navigation: {
@@ -40,4 +53,54 @@ var swiperSave = new Swiper(".save_swiper", {
         prevEl: ".swiper-prev",
     },
     speed: 800,
+})
+
+var swiperLife = new Swiper(".life_swiper", {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+    },
+    scrollbar: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    speed: 800,
+})
+
+var swiperTransportation = new Swiper(".transportation_swiper", {
+    slidesPerView: 3,
+    speed: 800,
+    loop: true,
+    // centeredSlides: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 18,
+        },
+    },
+})
+
+var swiperSchool = new Swiper(".school_swiper", {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+    },
+    scrollbar: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    speed: 800,
+})
+
+$(".school_text_tab_item").click(function(){
+    $(this).addClass("active").siblings(".school_text_tab_item").removeClass("active");
+    let li_index = $(this).index();
+    swiperSchool.slideTo(li_index, 1000, true);
 })
